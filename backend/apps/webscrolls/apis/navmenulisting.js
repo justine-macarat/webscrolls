ca403fe53406385b843796eaeb43b349d957a52b
justine-macarat <justine.macarat@tekmonks.com>
2020-12-05 01:26:40 +0800

    loop from existing item array instead of getting data again from function

--- backend/apps/webscrolls/apis/navmenulisting.js
+++ backend/apps/webscrolls/apis/navmenulisting.js
@@ -23,24 +23,24 @@ exports.doService = async jsonReq => {
         if(typeof (items[0]) == "object") menu.level1 = items;
         else {
             menu.level1 = [];
-            for (const entry of await readdirMenuOrderFileAsyncReturnDir(cmsPath, jsonReq.lang)) {
+            for (const entry of items) {
             if (entry.startsWith('.')) continue;    // hidden
 
-            let menu_entry = { item: entry, id: `${encodeURI(cmsPath + "/" + entry)}` };
-            let sub_entries = await readdirAsyncReturnOnlyDirs(`${cmsPath}/${entry}`, cmsPath);
-            if (sub_entries.length > 0) {
-                menu_entry.hasLevelTwo = true; menu_entry.level2 = []
-                for (let sub_entry of sub_entries) {
-                    const sub_menu = { item: sub_entry, id: `${encodeURI(cmsPath + "/" + entry + "/" + sub_entry)}` };
-                    try {
-                        const fileToRead = `${cmsPath}/${entry}/${sub_entry}/${getEntryi18nName(sub_entry, jsonReq.lang)}`;
-                        sub_menu.description = await readFileAsync(fileToRead, "utf8");
+                let menu_entry = { item: entry, id: `${encodeURI(cmsPath + "/" + entry)}` };
+                let sub_entries = await readdirAsyncReturnOnlyDirs(`${cmsPath}/${entry}`, cmsPath);
+                if (sub_entries.length > 0) {
+                    menu_entry.hasLevelTwo = true; menu_entry.level2 = []
+                    for (let sub_entry of sub_entries) {
+                        const sub_menu = { item: sub_entry, id: `${encodeURI(cmsPath + "/" + entry + "/" + sub_entry)}` };
+                        try {
+                            const fileToRead = `${cmsPath}/${entry}/${sub_entry}/${getEntryi18nName(sub_entry, jsonReq.lang)}`;
+                            sub_menu.description = await readFileAsync(fileToRead, "utf8");
+                        }
+                        catch (err) { sub_menu.description = sub_entry };
+                        menu_entry.level2.push(sub_menu);
                     }
-                    catch (err) { sub_menu.description = sub_entry };
-                    menu_entry.level2.push(sub_menu);
-                }
-            };
-            menu.level1.push(menu_entry);
+                };
+                menu.level1.push(menu_entry);
             }
         }
         return { result: true, menu };
@@ -66,7 +66,7 @@ async function readdirMenuOrderFileAsyncReturnDir(path, lang, files) {
             for (const subItemContent of itemContent.level1.level2) {
                 subItemContent.id = `${encodeURI(path + "/" + itemContent.level1.item + "/" + subItemContent.item)}`;
                 const fileToRead = `${path}/${itemContent.level1.item}/${subItemContent.item}/${getEntryi18nName(subItemContent.item, lang)}`;
-                subItem.description = await readFileAsync(fileToRead, "utf8");
+                subItemContent.description = await readFileAsync(fileToRead, "utf8");
             }
             files.push(itemContent.level1);
         }
